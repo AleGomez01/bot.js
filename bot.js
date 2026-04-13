1e5c4128bf319972347bc89694541bc6fa6eab78
Ale Gomez <gustavogomez.ale@gmail.com>
2026-04-13 19:36:08 -0300

    actualizacion

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot activo");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor vivo en puerto", PORT);
+});
+
 require('dotenv').config();
 
 const axios = require("axios");
